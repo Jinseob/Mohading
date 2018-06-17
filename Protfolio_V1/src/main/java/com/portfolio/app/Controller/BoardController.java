8003c29e6397894652500d389389a58ea40e13a8
LJS <LJS@DESKTOP-UIR0HSV>
2018-06-17 22:15:44 +0900

    2018.06.17 게시판 추가

--- Protfolio_V1/src/main/java/com/portfolio/app/Controller/BoardController.java
+++ Protfolio_V1/src/main/java/com/portfolio/app/Controller/BoardController.java
@@ -1,5 +1,7 @@
 package com.portfolio.app.Controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -9,9 +11,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.portfolio.app.Dao.CmmnDao;
+import com.portfolio.app.Model.B0010VO;
+import com.portfolio.app.Model.SearchOptionVO;
 
 @Controller
 public class BoardController {
@@ -21,10 +27,49 @@ public class BoardController {
 	private CmmnDao dao;
 	
 	@RequestMapping(value = "/b0010/b0010L.do")
-	public String boardMain(ModelMap model, HttpServletRequest request, 
-			HttpServletResponse response, HttpSession session) throws Exception{
+	public String boardMain(@ModelAttribute("searchOptionVO") SearchOptionVO searchOptionVO, ModelMap model, 
+			HttpServletRequest request, HttpServletResponse response, HttpSession session) throws Exception{
 		
+		@SuppressWarnings("unchecked")
+		List<B0010VO> list = (List<B0010VO>) dao.selectList("b0010.selectList_b0010", searchOptionVO);
+		model.addAttribute("list", list);
 		
 		return "b0010/b0010L";
+	}
+	
+	@RequestMapping(value = "/b0010/b0010D.do")
+	public String boardDetail(@ModelAttribute("b0010VO") B0010VO b0010VO, ModelMap model, 
+			HttpServletRequest request, HttpServletResponse response, HttpSession session) throws Exception{
+		
+		B0010VO detail = new B0010VO();
+		detail = (B0010VO) dao.select("b0010.selete_b0010", b0010VO);
+		model.addAttribute("detail", b0010VO);
+		
+		return "b0010/b0010D";
+	}
+	
+	@RequestMapping(value = "/b0010/b0010{procType}.do")
+	public String boardUpdate(@PathVariable String procType, @ModelAttribute("b0010VO") B0010VO b0010VO,
+			ModelMap model, HttpServletRequest request, HttpServletResponse response, HttpSession session) throws Exception{
+		
+//		여기 들어가기전에 session 정보가 있는지 확인 필요.
+		
+		String url = "";
+		
+		if(procType.equals("INS")) {
+			url = "i";
+			
+			dao.insert("b0010.insert_b0010", b0010VO);	// dao 에 null 값이 들어가있음. 왜일까.
+		}else if(procType.equals("UPD")) {
+			url = "u";
+			
+			dao.update("b0010.update_b0010", b0010VO);
+		}else if(procType.equals("DEL")) {
+			url = "l";
+			
+			dao.delete("b0010.delete_b0010", b0010VO);
+		}
+		
+		return "b0010/b0010" + url;
 	}
 }
--- Protfolio_V1/src/main/webapp/WEB-INF/views/common/taglib.jsp
+++ Protfolio_V1/src/main/webapp/WEB-INF/views/common/taglib.jsp
@@ -3,13 +3,13 @@
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
 
-<script src="/resources/js/jquery-3.3.1.min.js"></script>
+<!-- <script src="/resources/js/jquery-3.3.1.min.js"></script> -->
 <!-- Bootstrap core CSS -->
 <link href="/resources/vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
 
 <!-- Custom styles for this template -->
 <link rel="stylesheet" href="/resources/css/style.css" type="text/css">
-<link rel="stylesheet" type="text/css" href="/resources/font/font.css">
+<link rel="stylesheet" href="/resources/font/font.css" type="text/css">
  
  
 <!-- Bootstrap core JavaScript -->   
